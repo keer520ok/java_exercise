6f818687262e35bb9348f8d47bff327990f366df
keer520k <1943938670@qq.com>
2026-08-19 10:59:39 +0800

    秒数を時分秒に変換するプログラムを作成

---
+++ src/practice/basic/Ex04.java
@@ -0,0 +1,26 @@
+package practice.basic;
+
+import java.util.Scanner;
+
+/**
+ * 秒数を 時・分・秒 に変換するクラス
+ */
+public class Ex04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int time = sc.nextInt();
+
+        // 1時間 = 3600秒
+        int hours = time / 3600;
+        // 残りの秒数
+        int remainSeconds = time % 3600;
+        // 1分　= 60秒
+        int minutes = remainSeconds / 60;
+        int seconds = remainSeconds % 60;
+
+        System.out.println(hours + " " + minutes + " " + seconds);
+        sc.close();
+    }
+}
+
+
