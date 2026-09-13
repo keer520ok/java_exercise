712dcd87d34939232072a28ebb9439f0d5cdabb7
keer520k <1943938670@qq.com>
2026-09-13 15:31:12 +0800

    【改善】[practice.algorithm] Ex01 アルゴリズムの実装を最適化、ループ回数を削減 - 処理効率を向上、出力結果に変更なし

--- src/practice/algorithm/Ex01.java
+++ src/practice/algorithm/Ex01.java
@@ -28,10 +28,13 @@ public class Ex01 {
      * @return 素数ならtrue、そうでなければfalse
      */
     public static boolean isPrime(int number) {
-        int i = 2;
-        while (i <= Math.sqrt(number)) {
-            if (number % i == 0) return false;
-            i++;
+        if (number <= 1) return false;
+        if (number <= 3) return true;
+        if (number % 2 == 0 || number % 3 == 0) return false;
+        int i = 5;
+        while (i * i <= number) {
+            if (number % i == 0 || number % (i + 2) == 0) return false;
+            i += 6;
         }
         return true;
     }
